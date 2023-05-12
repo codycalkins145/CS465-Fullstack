2898792b050fecba89ec61ae45b9d9a7b5095e72
Cody Calkins <cody.calkins@qorvo.com>
2023-05-11 23:21:28 -0400

    JSON templates for Travel and Room pages

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
+const stays = JSON.parse(fs.readFileSync('./data/stays.json', 'utf8'))
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', {title: "Travlr Getaways - Rooms"});
+    res.render('rooms', {title: "Travlr Getaways - Rooms", stays});
 };
 
 module.exports = {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: "Travlr Getaways - Travel"});
+    res.render('travel', {title: "Travlr Getaways - Travel", trips});
 };
 
 module.exports = {
