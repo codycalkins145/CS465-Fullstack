9b975d6d2244486ca6536fa26c90d4700cdbc815
Cody Calkins <cody.calkins@qorvo.com>
2023-05-15 19:45:42 -0400

    JSON templates added for about and contact pages

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 const nav = JSON.parse(fs.readFileSync('./data/nav.json', 'utf8'));
+const details = JSON.parse(fs.readFileSync('./data/details.json', 'utf8'));
 
 /* GET about view */
 const about = (req, res) => {
-    res.render('about', {title: "Travlr Getaways - About", nav});
+    res.render('about', {title: "Travlr Getaways - About", nav, details});
 };
 
 module.exports = {
--- app_server/controllers/contact.js
+++ app_server/controllers/contact.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 
 const nav = JSON.parse(fs.readFileSync('./data/nav.json', 'utf8'));
+const info = JSON.parse(fs.readFileSync('./data/info.json', 'utf8'));
+const message = JSON.parse(fs.readFileSync('./data/message.json', 'utf8'));
 
 /* GET contact view */
 const contact = (req, res) => {
-    res.render('contact', {title: "Travlr Getaways - Contact", nav});
+    res.render('contact', {title: "Travlr Getaways - Contact", nav, message, info});
 };
 
 module.exports = {
