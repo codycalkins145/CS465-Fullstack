e1809cbfc3002d40b01c735c4089904d06443f29
Cody Calkins <cody.calkins@qorvo.com>
2023-05-24 18:58:42 -0400

    Removed stays db schema

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = 'mongodb://localhost/travlr'; 
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURI = `mongodb://${host}/travlr`; 
 const readLine = require('readline');
 
 mongoose.set('useUnifiedTopology', true);
--- app_api/models/travlr.js
+++ app_api/models/travlr.js
@@ -12,13 +12,4 @@ const tripSchema = new mongoose.Schema({
     description: {type: String, required: true} 
 }); 
 
-const staySchema = new mongoose.Schema({  
-    code: {type: String, required: true, index: true}, 
-    name: {type: String, required: true, index: true},  
-    image: {type: String, required: true}, 
-    description: {type: String, required: true},
-    rate: {type: String, required: true}
-});
-
 mongoose.model('trips', tripSchema);
-mongoose.model('stays', staySchema);
