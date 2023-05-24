324a7ecbcd691532962e1a214e8cf11627d5a42e
Cody Calkins <cody.calkins@qorvo.com>
2023-05-24 18:36:06 -0400

    Removed stays db schema

--- app_server/models/travlr.js
+++ app_server/models/travlr.js
@@ -13,14 +13,4 @@ const tripSchema = new mongoose.Schema({
     link: {type: String, required: true} 
 }); 
 
-const staySchema = new mongoose.Schema({  
-    code: {type: String, required: true, index: true}, 
-    name: {type: String, required: true, index: true},  
-    image: {type: String, required: true}, 
-    description: {type: String, required: true},
-    rate: {type: String, required: true},
-    link: {type: String, required: true} 
-});
-
 mongoose.model('trips', tripSchema);
-mongoose.model('stays', staySchema);
