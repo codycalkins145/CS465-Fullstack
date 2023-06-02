9cb24c875a16528dbaf0b1fc68dcbdb583c0db73
Cody Calkins <cody.calkins@qorvo.com>
2023-06-01 21:42:45 -0400

    Added logic for nav bar highlight depending on active page

--- app.js
+++ app.js
@@ -26,6 +26,48 @@ hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
+// Custom handlebar helper for various comparisons
+// Source: http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates
+hbs.registerHelper('compare', function (lvalue, operator, rvalue, options) {
+
+  var operators, result;
+  
+  if (arguments.length < 3) {
+      throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+  }
+  
+  if (options === undefined) {
+      options = rvalue;
+      rvalue = operator;
+      operator = "===";
+  }
+  
+  operators = {
+      '==': function (l, r) { return l == r; },
+      '===': function (l, r) { return l === r; },
+      '!=': function (l, r) { return l != r; },
+      '!==': function (l, r) { return l !== r; },
+      '<': function (l, r) { return l < r; },
+      '>': function (l, r) { return l > r; },
+      '<=': function (l, r) { return l <= r; },
+      '>=': function (l, r) { return l >= r; },
+      'typeof': function (l, r) { return typeof l == r; }
+  };
+  
+  if (!operators[operator]) {
+      throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+  }
+  
+  result = operators[operator](lvalue, rvalue);
+  
+  if (result) {
+      return options.fn(this);
+  } else {
+      return options.inverse(this);
+  }
+
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -41,7 +83,6 @@ app.use('/meals', mealsRouter);
 app.use('/news', newsRouter);
 app.use('/rooms', roomsRouter);
 app.use('/api', apiRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
