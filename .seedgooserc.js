ae377bd79b6a5405a68bdf625a9bb56c6828e09e
Earl Calkins <codycalkins145@gmail.com>
2023-06-26 20:43:02 -0400

    Updated db connection url

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -2,5 +2,5 @@ module.exports = {
     modelBaseDirectory: 'app_api/models', // model directory name
     models: ['*.js', '!db.js'], // model matcher
     data: 'data', // data directory name
-    db: 'mongodb://localhost:27017/travlr' // db connection url
+    db: 'mongodb://127.0.0.1:27017/travlr' // db connection url
 };
